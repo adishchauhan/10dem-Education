43e374d913dffc8d56d0d599e9e3e2d362891728
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2020-08-03 09:33:32 +0530

    First commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  jQuery(function($){
+    $('.navbar-toggle').click(function(){
+    $('.navbar-collapse').toggleClass('right');
+    $('.navbar-toggle').toggleClass('indexcity');
+    });
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+    $('#collapse-icon').toggleClass('fa-chevron-circle-left fa-chevron-circle-right');
+  });
+
+  $('.owl-carousel').owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    margin: 25,
+    responsive: {
+      0: {
+          items: 1
+      },
+      500: {
+          items: 2
+      },
+      900: {
+          items: 3
+      },
+      1200: {
+          items: 4
+      },
+      1600: {
+          items: 5
+      }
+    }
+  });
+    
+});
